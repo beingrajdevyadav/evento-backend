6d35381ad7516bb9eb76b0972a3e65fc76aa2866
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-28 21:07:26 +0530

    loginUser controller added.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -18,4 +18,24 @@ export const registerUser = async (req, res)=>{
     } catch (error) {
         res.status(500).json({error: "Server Error"});
     }
+};
+
+export const loginUser = async (req, res)=>{
+    const {email, password} = req.body;
+
+    try{
+
+        const user = await User.findOne({email});
+        if(!user) return res.status(404).json({message: "User not found"});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) return res.status(401).json({message: "Invalid credentials"});
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "1h"});
+
+        res.status(200).json({token, user: {id: user._id, name: user.name, email: user.email, role: user.role}  });
+    } catch(error){
+        res.status(500).json({error: "Login failed"});
+    }
 }
