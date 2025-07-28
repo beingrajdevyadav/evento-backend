f0e17c3131e14f96447c7628153ca1ec12e1e636
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-28 18:19:49 +0530

    db connection completed and app listening added.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // console.log("Jai Shree Shyam!");
-
 import { app } from "./src/app.js";
+import connectDB from "./src/db/db.js";
 
 const port = process.env.PORT || 8000;
 
+connectDB()
+.then(()=>{
 app.listen(port, ()=>{
     console.log(`server is serving at http://localhost:${port}`)
 });
+})
+.catch((error)=>{
+    console.log("MongoDB Connection Error : ", error);
+})
+
+
+
