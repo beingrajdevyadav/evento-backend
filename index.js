3cd3b3f356f61feedfcfe6130375ed4cc5a623ad
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-28 18:02:42 +0530

    mongodb atlas setup completed.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Jai Shree Shyam!");
+// console.log("Jai Shree Shyam!");
+
+import { app } from "./src/app.js";
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, ()=>{
+    console.log(`server is serving at http://localhost:${port}`)
+});
