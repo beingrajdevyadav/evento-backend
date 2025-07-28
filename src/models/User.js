a5b5cd6297122f1b8f0da65364cf06039b4438ad
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-28 18:58:52 +0530

    user model created.

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        min: 3,
+        max: 25,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["admin", "organizer", "attendee"],
+        default: "attendee",
+    },
+    profilePic:{
+        type: String,
+    }
+}, {timestamps: true});
+
+export default User = mongoose.model("User", userSchema);
