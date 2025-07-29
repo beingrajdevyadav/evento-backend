3dd015f9b912e344a1d2c4ac5fd47536127b751f
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-29 17:02:52 +0530

    home route response added.

--- src/app.js
+++ src/app.js
@@ -13,9 +13,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/bookings", bookingRoutes);
 
+app.get("/", (res, req)=>{
+    res.status(200).json({
+        message: "Welcome to Evento API",
+        version: "1.0.0",
+        endpoints: {
+            auth: "/api/auth",
+            events: "/api/events",
+            bookings: "/api/bookings"
+        },
+        author: "Rajdev Yadav",
+        github: "https://github.com/beingrajdevyadav/evento-backend",
+        linkedin: "https://www.linkedin.com/in/rjd06/",
+        description: "Evento is a ticket booking system for events, allowing users to book tickets and generate QR codes for their bookings."
+    });
+})
 
 export {app};
