80500da751d8a26438a32243846d5586e6d71d12
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-29 17:08:12 +0530

    typeError fixed.

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/bookings", bookingRoutes);
 
-app.get("/", (res, req)=>{
+app.get("/", (req, res)=>{
     res.status(200).json({
         message: "Welcome to Evento API",
         version: "1.0.0",
