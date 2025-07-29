5eb691de7507dac0eaab283717feb689aa079caa
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-29 09:31:45 +0530

    get all events controller added.

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -1,8 +1,9 @@
 import Event from "../models/Event.js";
 
-export const createEvent = async (req, res)=>{
+// to create event
+export const createEvent = async (req, res) => {
     try {
-        const event = new Event({...req.body, createdBy: req.user.id});
+        const event = new Event({ ...req.body, createdBy: req.user.id });
         const savedEvent = await event.save();
 
         res.status(201).json(savedEvent);
@@ -12,3 +13,14 @@ export const createEvent = async (req, res)=>{
         })
     }
 };
+
+
+// to get all events
+export const getAllEvents = async (req, res) => {
+    try {
+        const events = await Event.find().populate("createdBy", "name email");
+        res.status(200).json(events);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to fetch events" });
+    }
+}
